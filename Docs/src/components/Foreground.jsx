3df13b18f778594468bbe0f32d4811cc68531580
fakhir96 <fakhir9101@gmail.com>
2025-10-23 12:46:20 +0500

    Minor Bug Fix

--- Docs/src/components/Foreground.jsx
+++ Docs/src/components/Foreground.jsx
@@ -26,12 +26,20 @@ function Foreground() {
 
     ]
   
+
+
     return (
+        <>
     <div ref={ref} className='fixed z-[3] w-full h-full top-0 left-0 flex gap-7 flex-wrap p-5' >
         {data.map((item, index)=> (
             <Card data={item} reference={ref}/>
         ))}
     </div>
+        <button
+        // onClick={addTask} 
+        className='bg-red-400 absolute right-20 top-20 px-4 py-2 text-sm'>Add</button>
+    
+        </>
 
   )
 }
